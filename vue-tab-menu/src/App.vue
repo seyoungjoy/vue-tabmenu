93fe4c003d03036e5eef39a98a5324975b10f90f
seyoungjoy <diddpwl80@gmail.com>
2022-02-21 18:10:01 +0900

    create tab menu

--- vue-tab-menu/src/App.vue
+++ vue-tab-menu/src/App.vue
@@ -1,18 +1,63 @@
 <template>
-  <div id="app">
-      <p></p>
-  </div>
+    <div id="app">
+        <TabNav
+            :tabs="['Home', 'Setting', 'Profile']"
+            :selected="selected"
+            @selected="setSelected"
+        >
+            <Tab :isSelected="selected === 'Home'">
+                <p>Some test text</p>
+
+            </Tab>
+            <Tab :isSelected="selected === 'Settings'">
+                <h1>more test text</h1>
+            </Tab>
+            <Tab :isSelected="selected === 'Profile'">
+                <ul>
+                    <li>list test1</li>
+                    <li>list test2</li>
+                    <li>list test3</li>
+                    <li>list test4</li>
+                </ul>
+            </Tab>
+
+        </TabNav>
+
+    </div>
 </template>
 
 <script>
 
+import TabNav from "@/components/TabNav";
+import Tab from "@/components/Tab";
+
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    data(){
+      return{
+          selected:'Home'
+      }
+    },
+    components: {
+        Tab,
+        TabNav
+    },
+    methods:{
+        setSelected(tab){
+            this.selected = tab;
+        }
+    }
 }
 </script>
 
 <style>
+body{
+    background-color: #45443F;
+    color:#fefefe;
+}
+th, td{
+    padding:5px;
+    border:1px solid #fff;
+}
 
 </style>
---
+++ vue-tab-menu/src/components/TabNav.vue
@@ -0,0 +1,44 @@
+<template>
+    <div style="width:30%">
+        <ul class="nav nav-tabs">
+            <li v-for="tab in tabs" :key="tab" class="nav-item">
+                <a class="nav-link"
+                   :class="{active:tab === selected}"
+                    @click="setTab(tab)"
+                ></a>
+<!--                tab이랑 selected랑 같을 때 active클래스를 바인딩해줘라.-->
+                {{tab}}
+            </li>
+        </ul>
+
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props:{
+        tabs:{
+            type:Array,
+            required:true,
+        },
+        selected:{
+            type:String,
+            required: true,
+        }
+    },
+    methods:{
+        setTab(tab){
+            this.$emit('selected', tab);
+
+        }
+    }
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
